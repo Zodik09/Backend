6dabcf015a09337681c99ba10697fc1ed324c33a
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-09-26 18:34:15 +0530

    Secure DB's password by using dotenvx.

--- Day 5/src/db/db.js
+++ Day 5/src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('@dotenvx/dotenvx').config()
 
 const connectToDB = () => {
-    mongoose.connect('mongodb+srv://zodik:tuRL7sg0JQlqCaBq@cluster.51yornv.mongodb.net/zodik')
+    mongoose.connect(process.env.MONGO_URI)
         .then(() => {
             console.log('DB has been connected to server successfully.');
         })
