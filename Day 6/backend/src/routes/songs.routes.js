c724e5624172876e63e0c7ed8424a3659ca10db1
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-09-30 18:39:29 +0530

    Remove few console.logs.

--- Day 6/backend/src/routes/songs.routes.js
+++ Day 6/backend/src/routes/songs.routes.js
@@ -7,9 +7,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/songs", upload.single("audio"), async (req, res) => {
     console.log(req.body);
-    // console.log(req.file);
     const fileData = await uploadFile(req.file);
-    console.log(fileData);
     
     res.json({
         message: "New Song Posted successfully."
