043cc625e794d99738585c1c2abef3064f85507e
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-10-12 12:53:12 +0530

    Add cookieParser middleware in to app.js file. Download multer, imagekit packages.

--- Day 8 - Social Media/backend/server.js
+++ Day 8 - Social Media/backend/server.js
@@ -1,10 +1,9 @@
-const app = require('./src/app');
-const connectToDB = require('./src/db/db');
+const app = require("./src/app");
+const connectToDB = require("./src/db/db");
 require("@dotenvx/dotenvx").config();
 
 connectToDB();
 
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000...');
-})
+  console.log("Server is running on port 3000...");
+});
--- Day 8 - Social Media/backend/src/db/db.js
+++ Day 8 - Social Media/backend/src/db/db.js
@@ -1,13 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToDB = () => {
-    mongoose.connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((err) => {
-            console.log('Error connecting to MongoDB:', err);
-        });
-}
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.log("Error connecting to MongoDB:", err);
+    });
+};
 
 module.exports = connectToDB;
