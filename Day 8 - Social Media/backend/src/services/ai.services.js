ec3e19ec8adffcb8e3ad74b592af698fff26cfe6
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-10-12 13:41:54 +0530

    Install @google/genai package for ai integration. Complete Gemini API integration in ai.services.js folder

---
+++ Day 8 - Social Media/backend/src/services/ai.services.js
@@ -0,0 +1,15 @@
+const { GoogleGenAI } = require("@google/genai");
+require("@dotenvx/dotenvx").config();
+
+// The client gets the API key from the environment variable `GEMINI_API_KEY`.
+const ai = new GoogleGenAI({});
+
+async function main(url) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Write a single caption for the given image: " + url + ". Make it catchy and relevant to social media."
+  });
+  return response.text
+}
+
+module.exports = main;
