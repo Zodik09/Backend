69429d4d781d503627eb6b9c851baa3e4d258661
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2025-10-14 19:51:02 +0530

    Add cors middleware in the Social Media Backend.

--- Day 8 - Social Media/backend/src/app.js
+++ Day 8 - Social Media/backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth.route");
 const postRoute = require("./routes/post.route");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 
